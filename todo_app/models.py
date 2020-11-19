505c82574b39d8d8b722313b57083941f88801b7
houston.fortney <houston.fortney@gmail.com>
2020-11-18 22:07:58 -0500

    Make tasks expire after 30 days to keep db small.

--- todo_app/models.py
+++ todo_app/models.py
@@ -16,3 +16,9 @@ class Task(db.Document):
     notes = me.StringField(default='')
     priority = me.StringField(required=True)
     created = me.DateTimeField()
+
+    meta = {
+        'indexes': [
+            {'fields': ['created'], 'expireAfterSeconds': 60 * 60 * 24 * 30}  # For keeping demo db small
+        ]
+    }
