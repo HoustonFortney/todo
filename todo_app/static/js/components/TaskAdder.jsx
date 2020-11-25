6d92cee3673fe8350a26aaf0b39652aca36e0170
houston.fortney <houston.fortney@gmail.com>
2020-11-25 13:30:38 -0500

    Lint javascript files

---
+++ todo_app/static/js/components/TaskAdder.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {Button, Form, ListGroup} from "react-bootstrap";
+
+class TaskAdder extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.onCreate = this.onCreate.bind(this);
+
+    this.state = {
+      name: ""
+    };
+  }
+
+  handleInputChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleKeyDown(event) {
+    if (event.key === "Enter") {
+      this.onCreate();
+
+      // Clear native autocomplete
+      event.target.blur();
+      event.target.focus();
+    }
+  }
+
+  onCreate() {
+    if (this.state.name) {
+      this.props.onCreate(this.state);
+    }
+    this.setState({name: ""})
+  }
+
+  render() {
+    return (
+      <ListGroup.Item>
+        <Form id="task-adder" inline onSubmit={(event) => event.preventDefault()}>
+          <span className="btn"><i className="fas fa-plus"/></span>
+          <Form.Control
+            id="task-name"
+            className="w-75"
+            placeholder="New task"
+            name="name"
+            autoComplete="off"
+            value={this.state.name}
+            onKeyDown={this.handleKeyDown}
+            onChange={this.handleInputChange}
+          />&nbsp;
+          <Button id="create-task-button" type="submit" onClick={this.onCreate}>
+            Create
+          </Button>
+        </Form>
+      </ListGroup.Item>
+    );
+  }
+}
+
+export default TaskAdder;
