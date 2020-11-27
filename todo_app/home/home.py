fb45a07e04ebf840c32c7100b6e3f32f56ba8fad
houston.fortney <houston.fortney@gmail.com>
2020-11-27 12:43:04 -0500

    Add credits page

--- todo_app/home/home.py
+++ todo_app/home/home.py
@@ -1,12 +1,22 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+import json
 
 home_blueprint = Blueprint('home_blueprint', __name__, template_folder='templates')
+
+
+with open('credits.json') as credits_file:
+    credit_items = json.load(credits_file)
 
 
 @home_blueprint.route('/')
 def home():
     return render_template('home/index.jinja2')
+
+
+@home_blueprint.route('/credits')
+def credits():
+    return render_template('home/credits.jinja2', credit_items=credit_items)
 
 
 @home_blueprint.route('/<name>')
