7c5f493b7584a7802706428743770a6fac3f9d3e
houston.fortney <houston.fortney@gmail.com>
2020-11-28 18:47:25 -0500

    Fix remote address storage

--- todo_app/demo_login_system.py
+++ todo_app/demo_login_system.py
@@ -15,7 +15,7 @@ def load_user(request):
 
     if user is None:
         # User does not exist, create new demo user
-        user = User(username='Demo User', addr=request.remote_addr)
+        user = User(username='Demo User', addr=request.access_route[0])
         user.save()
         session['user_id'] = str(user.id)
 
