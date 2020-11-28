6113b5ca29deadc74f1a7e1a8b8e902d240f1802
houston.fortney <houston.fortney@gmail.com>
2020-11-28 17:17:44 -0500

    Get secrets on app init

--- application.py
+++ application.py
@@ -1,7 +1,18 @@
 from todo_app import create_app
 from config import ProductionConfig
+import boto3
 
-application = create_app(ProductionConfig)
+# Get secret config from s3 bucket
+s3 = boto3.client('s3')
+s3.download_file('tododemo-secrets', 'secrets.py', 'secrets.py')
+from secrets import SecretConfig
+
+
+class ProductionConfigWithSecrets(SecretConfig, ProductionConfig):
+    pass
+
+
+application = create_app(ProductionConfigWithSecrets)
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
