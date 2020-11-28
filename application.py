38a2d58fcad94f4b27951c786ca3ad498e642b5b
houston.fortney <houston.fortney@gmail.com>
2020-11-28 18:49:22 -0500

    Refactor bucket name into config

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import boto3
 
 # Get secret config from s3 bucket
 s3 = boto3.client('s3')
-s3.download_file('tododemo-secrets', 'secrets.py', 'secrets.py')
+s3.download_file(ProductionConfig.SECRETS_BUCKET_NAME, 'secrets.py', 'secrets.py')
 from secrets import SecretConfig
 
 
