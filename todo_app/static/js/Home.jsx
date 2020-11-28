5df18f4938a41ba7089c3a1bc8ed6c44b1394287
houston.fortney <houston.fortney@gmail.com>
2020-11-28 14:36:19 -0500

    Remove fontawesome and bootstrap from bundle

--- todo_app/static/js/Home.jsx
+++ todo_app/static/js/Home.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
 import TaskList from './components/TaskList';
 import Footer from './components/Footer';
-import './main.sass';
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
 
 const Home = () => (
   <Container className="d-flex flex-column min-vh-100">
