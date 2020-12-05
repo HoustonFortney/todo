984b864da2e34258c38c4598d82b6be76dfb1a1b
houston.fortney <houston.fortney@gmail.com>
2020-12-04 19:45:14 -0500

    Add some padding on the top

--- todo_app/static/js/Home.jsx
+++ todo_app/static/js/Home.jsx
@@ -5,7 +5,7 @@ import TaskList from './components/TaskList';
 import Footer from './components/Footer';
 
 const Home = () => (
-  <Container className="d-flex flex-column min-vh-100">
+  <Container className="d-flex flex-column min-vh-100 pt-2">
     <h1>Tasks</h1>
     <TaskList />
     <Footer />
