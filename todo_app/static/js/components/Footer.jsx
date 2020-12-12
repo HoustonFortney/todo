5b7872276a3b9af31264bbe95b8c4763cd7fb1d0
houston.fortney <houston.fortney@gmail.com>
2020-12-11 23:28:10 -0500

    Concise name for privacy policy

--- todo_app/static/js/components/Footer.jsx
+++ todo_app/static/js/components/Footer.jsx
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 const Footer = () => (
   <div className="mt-auto d-flex justify-content-center">
     <Button variant="link" href="/terms">Terms</Button>
-    <Button variant="link" href="/privacy">Privacy Policy</Button>
+    <Button variant="link" href="/privacy">Privacy</Button>
     <Button variant="link" href="/cookies">Cookies</Button>
     <Button variant="link" href="/credits">Credits</Button>
   </div>
